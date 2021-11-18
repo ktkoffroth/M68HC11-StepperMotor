4f252e8e9d9694a5066f2437b1f043ee1703aff7
Jamie-Ni <81156914+Jamie-Ni@users.noreply.github.com>
2021-11-18 12:57:29 -0500

    Updated final version

--- stepper.asm
+++ stepper.asm
@@ -10,19 +10,20 @@ OC1M    EQU $100C ; Output Compare 1 Mask Register
 OC1D    EQU $100D ; Output Compare 1 Data Register (Stepper Control Pattern)
 TMSK1   EQU $1022 ; Timer Mask Register
 TFLG1   EQU $1023 ; Timer Interrupt Flag Register
-TCNT    EQU $100E ; Free Running Counter
+PORTB   EQU $1004 ; Port B
 FULLSTEP EQU 4 ; Sequence Size (in bytes) for Full Step
 HALFSTEP EQU 8 ; Sequence Size (in bytes) for Half Step
 
         ORG $D500 ; Constants and Variables
 
 ; Variables
-PWIDTH RMB 2 ; 2 Bytes for Pulse Width of Stepper
+DELTA RMB 2 ; 2 Bytes for Pulse Width of Stepper
 STEPPTR RMB 2 ; Pointer variable for the step sequence
 SEQSIZE RMB 1 ; Variable for Sequence Size in bytes (4 for Full Step, 8 for Half Step)
+COUNT RMB 1 ; Counter Variable
 
 ; Constants
-FCW FCB $A0,$90,$50,$60 ; Full Step Clockwise Sequence
+FCW FCB $50,$48,$28,$30 ; Full Step Clockwise Sequence
 FCCW FCB $60,$50,$90,$A0 ; Full Step Counter Clockwise Sequence
 HCW FCB $A0,$80,$90,$10,$50,$40,$60,$20 ; Half Step Clockwise Sequence
 HCCW FCB $20,$60,$40,$50,$10,$90,$80,$A0 ; Half Step Counter Clockwise Sequence
@@ -38,7 +39,7 @@ HCCW FCB $20,$60,$40,$50,$10,$90,$80,$A0 ; Half Step Counter Clockwise Sequence
 ; Init Interrupt Timer
 OC1INIT:
         CLI ; Enable Interrupts System-Wide
-        LDAA #$F0
+        LDAA #$78
         STAA OC1M ; Enable OC1
         LDAA #$80
         STAA TMSK1 ; Enable OC1 Interrupts
@@ -47,11 +48,11 @@ OC1INIT:
 ; Additional Init
 INIT:
         LDD #FCW ; Load the address of FCW
-        STD STEPPTR ; Store addr to STEPPTR
-        LDD #$5168 ; Set PWIDTH for testing (RPM 30)
-        STD PWIDTH
-        LDAA #FULLSTEP ; Set Sequence Size to 4 Bytes
-        STAA SEQSIZE
+        STD STEPPTR ; Store address to STEPPTR
+        LDD #$FFFF ; Set DELTA for testing (RPM 30)
+        STD DELTA
+        LDAA #5 ; Set Count to 4
+        STAA COUNT
 
 MAIN:
         BRA MAIN ; While(1)
@@ -60,20 +61,21 @@ MAIN:
 ; Set TOC1 and OC1D (Interrupt Service Routine)
 SETOC1:
 ; Set TOC1 Register
-        LDD TCNT ; Load the current free running counter value
-        ADDD PWIDTH ; Add the desired PWIDTH
+        LDD TOC1 ; Load the current Output Compare value
+        ADDD DELTA ; Add the desired DELTA
         STD TOC1 ; And store to Timer Output Compare 1
 
 ; Set OC1D Register
         LDX STEPPTR
-        LDD #FCW ; Check if sequence has finished
-        ADDD SEQSIZE
-        CPD STEPPTR
+        DEC COUNT ; Decrement Counter
         BNE SETOC1D ; if((*FCW + sizeof(FCW)) > STEPPTR)
-        LDX FCW ; else, reset X to FCW
+        LDX #FCW ; else, reset X to FCW
+        LDAA #4 ; Set Count back to 4
+        STAA COUNT
 SETOC1D:
         LDAA 0,X ; Update OC1D to next Step Sequence
         STAA OC1D
+        STAA PORTB
         INX ; Increment Pointer
         STX STEPPTR
 
